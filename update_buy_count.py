46060ee4b048cee153ea210f65565f95f3c7ad1e
koko-pearl-j-dev <jplus0401jo2@gmail.com>
2026-08-17 16:47:21 +0900

    Add status filter to orders query

--- update_buy_count.py
+++ update_buy_count.py
@@ -22,7 +22,7 @@ def get_orders():
         after = f', after: "{cursor}"' if cursor else ""
         query = f"""
         {{
-        　orders(first: 250, query: "created_at:>{since}"{after}) {{
+        　orders(first: 250, query: "created_at:>{since} status:any"{after}) {{
             edges {{
               node {{
                 lineItems(first: 50) {{
